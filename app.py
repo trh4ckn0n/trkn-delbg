dc7aa456aa6a0afdd933e214b4ae9c1ba8a5f1bf
trh4ckn0n <trhacknon@proton.me>
2025-06-22 01:54:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -133,4 +133,5 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
