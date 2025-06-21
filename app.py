edd59c0f9e440672e11acb0096a4035db8279ac2
trh4ckn0n <trhacknon@proton.me>
2025-06-22 01:41:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
